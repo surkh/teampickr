deaf61b5a0b9ede428a5331c88f1497ae3960a57
Surkhab Niazi <sniazi@yp.com>
2016-02-19 08:41:52 -0800

    Test servlet

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sn6628 on 2/19/2016.
+ */
+public class Test extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello");
+    }
+}
