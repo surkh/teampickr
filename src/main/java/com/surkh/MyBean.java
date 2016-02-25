cb93c92ee5221438fa8da85633f12d420c16054b
Surkhab Niazi <sniazi@yp.com>
2016-02-25 11:41:30 -0800

    Updated team list

---
+++ src/main/java/com/surkh/MyBean.java
@@ -0,0 +1,14 @@
+package com.surkh;
+
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
