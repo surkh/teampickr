46a521a423820c79188689da0ecb05cef3800ce7
Surkhab Niazi <sniazi@yp.com>
2016-03-08 01:15:54 -0800

    Autoload player list from api call instead of JS hard-coding

--- src/main/java/com/surkh/Teams.java
+++ src/main/java/com/surkh/Teams.java
@@ -21,23 +21,53 @@ import javax.inject.Named;
 )
 public class Teams {
 
-  public static ArrayList<HelloGreeting> greetings = new ArrayList<HelloGreeting>();
+  public static ArrayList<HelloGreeting> players = new ArrayList<HelloGreeting>();
 
   static {
-    greetings.add(new HelloGreeting("surkh"));
-    greetings.add(new HelloGreeting("darin"));
+    players.add(new HelloGreeting("surkh"));
+    players.add(new HelloGreeting("darin"));
+    players.add(new HelloGreeting("Abhishek"));
+    players.add(new HelloGreeting("Al"));
+    players.add(new HelloGreeting("Aleks"));
+    players.add(new HelloGreeting("Anthony"));
+    players.add(new HelloGreeting("Chun"));
+    players.add(new HelloGreeting("Dan"));
+    players.add(new HelloGreeting("Darin"));
+    players.add(new HelloGreeting("DavidC"));
+    players.add(new HelloGreeting("DavidH"));
+    players.add(new HelloGreeting("DavidS"));
+    players.add(new HelloGreeting("Dom"));
+    players.add(new HelloGreeting("Ed"));
+    players.add(new HelloGreeting("Eric"));
+    players.add(new HelloGreeting("Gary"));
+    players.add(new HelloGreeting("Gowri"));
+    players.add(new HelloGreeting("Marcos"));
+    players.add(new HelloGreeting("MichaelS"));
+    players.add(new HelloGreeting("MikeH"));
+    players.add(new HelloGreeting("Ming"));
+    players.add(new HelloGreeting("NickH"));
+    players.add(new HelloGreeting("NikK"));
+    players.add(new HelloGreeting("Paul"));
+    players.add(new HelloGreeting("Peter"));
+    players.add(new HelloGreeting("Raghu"));
+    players.add(new HelloGreeting("Rudy"));
+    players.add(new HelloGreeting("Senthil"));
+    players.add(new HelloGreeting("Sibiao"));
+    players.add(new HelloGreeting("Surkhab"));
+    players.add(new HelloGreeting("Tanvir"));
+    players.add(new HelloGreeting("Zareh"));
   }
 
   public HelloGreeting getGreeting(@Named("id") Integer id) throws NotFoundException {
     try {
-      return greetings.get(id);
+      return players.get(id);
     } catch (IndexOutOfBoundsException e) {
       throw new NotFoundException("Greeting not found with an index: " + id);
     }
   }
 
   public ArrayList<HelloGreeting> listGreeting() {
-    return greetings;
+    return players;
   }
 
   @ApiMethod(name = "greetings.multiply", httpMethod = "post")
